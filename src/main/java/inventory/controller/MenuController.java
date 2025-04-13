394048fc846d3258671107b555c9759d132edbd3
Dương Trí Nguyên <21130456@st.hcmuaf.edu.vn>
2025-04-13 16:21:15 +0700

    build module menu

---
+++ src/main/java/inventory/controller/MenuController.java
@@ -0,0 +1,55 @@
+package inventory.controller;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import inventory.model.Auth;
+import inventory.model.Menu;
+import inventory.model.Paging;
+import inventory.model.Role;
+import inventory.service.MenuService;
+import inventory.service.RoleService;
+
+@Controller
+public class MenuController {
+	@Autowired
+	private MenuService menuService;
+	@Autowired
+	private RoleService roleService;
+
+	@GetMapping("/menu/list/{page}")
+	public String menuList(Model model, @PathVariable("page") int page, @ModelAttribute("searchForm") Menu menu) {
+		Paging paging = new Paging(10);
+		paging.setIndexPage(page);
+		List<Menu> menuList = menuService.getListMenu(paging, menu);
+		List<Role> roles = roleService.getRoleList(null, null);
+		Collections.sort(roles, (o1, o2) -> o1.getId() - o2.getId());
+
+		for (Menu item : menuList) {
+			Map<Integer, Integer> mapAuth = new TreeMap<>();
+			for (Role role : roles) {
+				mapAuth.put(role.getId(), 0);
+			}
+			for (Object obj : item.getAuths()) {
+				Auth auth = (Auth) obj;
+				mapAuth.put(auth.getRole().getId(), auth.getPermission());
+			}
+			item.setMapAuth(mapAuth);
+		}
+
+		model.addAttribute("pageInfo", paging);
+		model.addAttribute("menuList", menuList);
+		model.addAttribute("roles", roles);
+
+		return "menu-list";
+	}
+}
